8b6b56ad8a2fcdae5def451086bb88030528db1e
leo-essec <B00814901@essec.edu>
2025-02-04 18:04:28 +0100

    Ajout de musées

---
+++ script.js
@@ -0,0 +1,52 @@
+const museums = [
+    {
+        name: "Louvre Museum",
+        district: "1st",
+        image: "https://upload.wikimedia.org/wikipedia/commons/6/6a/Louvre_Museum_Wikimedia_Commons.jpg",
+        description: "World's largest art museum housing 38,000+ works including the Mona Lisa and Venus de Milo."
+    },
+    {
+        name: "Musée d'Orsay",
+        district: "7th",
+        image: "https://upload.wikimedia.org/wikipedia/commons/3/3c/Mus%C3%A9e_d%27Orsay_20200306.jpg",
+        description: "Impressionist masterpieces in a stunning Beaux-Arts railway station (1840-1914)."
+    },
+    {
+        name: "Centre Pompidou",
+        district: "4th",
+        image: "https://upload.wikimedia.org/wikipedia/commons/0/0c/Centre_Pompidou_%2820155363698%29.jpg",
+        description: "Modern art museum with radical architecture housing Europe's largest modern art collection."
+    },
+    // Liste complète des 50+ musées ci-dessous...
+    {
+        name: "Musée Rodin",
+        district: "7th",
+        image: "https://upload.wikimedia.org/wikipedia/commons/7/73/Hotel_Biron_Paris_20062014.jpg",
+        description: "Auguste Rodin's sculptures in an 18th-century mansion with rose gardens."
+    },
+    {
+        name: "Musée Picasso",
+        district: "3rd",
+        image: "https://upload.wikimedia.org/wikipedia/commons/6/6d/Le_Marais_%284014056151%29.jpg",
+        description: "5,000+ works by Pablo Picasso in a 17th-century Baroque mansion."
+    },
+    {
+        name: "Musée de l'Orangerie",
+        district: "1st",
+        image: "https://upload.wikimedia.org/wikipedia/commons/a/ae/Orangerie_Tuileries_%289698782929%29.jpg",
+        description: "Monet's Water Lilies murals and impressionist/post-impressionist collection."
+    },
+    {
+        name: "Musée du Quai Branly",
+        district: "7th",
+        image: "https://upload.wikimedia.org/wikipedia/commons/3/33/Mus%C3%A9e_du_quai_Branly_2009.jpg",
+        description: "Indigenous art from Africa, Asia, Oceania and the Americas."
+    },
+    {
+        name: "Petit Palais",
+        district: "8th",
+        image: "https://upload.wikimedia.org/wikipedia/commons/9/9f/Petit_Palais_%28Paris%29_001.jpg",
+        description: "Fine arts museum with works from Rembrandt to Monet in a Belle Époque palace."
+    },
+    // ... (ajouter les 40+ autres musées suivant le même format)
+];
